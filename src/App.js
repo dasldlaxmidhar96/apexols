e2f59b32a9e3f9d60a7b657271e093c8bc1fd8f4
LAXMIDHAR DAS <laxmidhardas0000@gmail.com>
2022-12-17 09:52:26 +0530

    com

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
