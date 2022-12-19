9bef54c7e0b0ade3efe996cc52b1bd25d5b72741
LAXMIDHAR DAS <laxmidhardas0000@gmail.com>
2022-12-19 14:03:05 +0530

    all

--- src/Pages/MedicalHistoryPage/TayerOrganInvolvement.js
+++ src/Pages/MedicalHistoryPage/TayerOrganInvolvement.js
@@ -3,35 +3,35 @@ import * as React from 'react';
 export default function RowRadioButtonsGroup() {
   return (
     <>
-   <div  className='tdiv'>
+   <div>
         
         <div>
-            <p className='fine tayHei'>Tayer Organ Involvement</p>
+            <p>Tayer Organ Involvement</p>
         </div>
-        <div className='renal'>
+        <div >
         <ul>
-          <div className='alco eyes'><span className='alco eyes'>Eyes - Ratinopathy</span></div>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Edema Legs</span></a></li>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Facial Puffiness</span></a></li>     
-            <li><a><input type="checkbox" /><span className="fsizekliye">Poor Urinary Sheam</span></a></li>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Recurrent UTI</span></a></li>
+          <div><span>Eyes - Ratinopathy</span></div>
+            <li><a><input type="checkbox" /><span>Edema Legs</span></a></li>
+            <li><a><input type="checkbox" /><span>Facial Puffiness</span></a></li>     
+            <li><a><input type="checkbox" /><span>Poor Urinary Sheam</span></a></li>
+            <li><a><input type="checkbox" /><span>Recurrent UTI</span></a></li>
         </ul>
         </div>
-        <div className='renal last'>
+        <div>
         <ul>
-          <div className='alco eyes'><span className='alco eyes'>Eyes - Laser Photocaullation</span></div>
-            <li><a><input type="checkbox" /><span className="fsizekliye">No</span></a></li>
-            <li><a><input type="checkbox" /><span className="fsizekliye">RE</span></a></li>     
-            <li><a><input type="checkbox" /><span className="fsizekliye">LE</span></a></li>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Bilateral</span></a></li>
+          <div><span>Eyes - Laser Photocaullation</span></div>
+            <li><a><input type="checkbox" /><span>No</span></a></li>
+            <li><a><input type="checkbox" /><span>RE</span></a></li>     
+            <li><a><input type="checkbox" /><span>LE</span></a></li>
+            <li><a><input type="checkbox" /><span>Bilateral</span></a></li>
         </ul>
         </div>
-        <div className='renal atlast'>
+        <div >
         <ul>
-          <div className=' alco eyes '><span className='alco eyes'>Eyes - Vision</span></div>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Good</span></a></li>
-            <li><a><input type="checkbox" /><span className="fsizekliye">Impaired</span></a></li>     
-            <li><a><input type="checkbox" /><span className="fsizekliye">Blind</span></a></li>
+          <div ><span>Eyes - Vision</span></div>
+            <li><a><input type="checkbox" /><span>Good</span></a></li>
+            <li><a><input type="checkbox" /><span>Impaired</span></a></li>     
+            <li><a><input type="checkbox" /><span>Blind</span></a></li>
         </ul>
         </div>
         </div>
